57ade2b7224d8e894ca5f71d4da6c811a09333c9
sleex89 <samlee723@gmail.com>
2018-04-26 15:21:24 -0700

    Finished up to fizz

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  for(let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+  let max = 0;
+  let result;
+  for(var key in chars) {
+    if(chars[key] > max) {
+      max = chars[key];
+      result = key
+    }
+  }
+  return result;
+}
 
 module.exports = maxChar;
--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - 1 - i]
+  })
+}
 
 module.exports = palindrome;
+
+
+// function palindrome(str) {
+//   if(str.split('').reverse().join('') === str) {
+//     return true;
+//   }
+//   return false;
+// }
